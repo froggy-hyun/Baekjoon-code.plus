365c7cf3cfae1c9f8c5e09e56b1c0817f6b8e721
Evan Choi (최성현) <111727754+froggy-hyun@users.noreply.github.com>
2025-01-26 17:33:33 +0900

    [Silver III] Title: 1, Time: 140 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.py
@@ -0,0 +1,17 @@
+# import sys
+# input = sys.stdin.readline()
+
+while(True): # 입력 개수 안 알려줌
+    try: # 종료조건이 따로 주어져있지 않으므로 try except를 이용해 예외처리를 해주어 프로그램이 종료
+        n = int(input())
+
+    except:
+        break
+    
+    m = 1
+    count = 1
+    while(m % n != 0):
+        m = (m*10)+1
+        count += 1
+    
+    print(count)
